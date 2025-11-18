7bb0600244c023616230750fe67a988b1b43168e
Baotq1406 <quocbaotk1406@gmail.com>
2025-11-18 13:43:47 +0700

    add script CameraController: Camera can follow the player

---
+++ Assets/00 SCRIPTS/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform _player;
+    [SerializeField] Vector2 _camOffset;
+
+    void Start()
+    {
+        //_player = GameManager.Instance.Player.transform;
+    }
+
+    void Update()
+    {
+        //Vector3 newPos = new Vector3(_player.position.x, _player.position.y, this.transform.position.z);
+        //this.transform.position = newPos;
+
+        Vector3 pos = _player.position + (Vector3)_camOffset;
+        pos.z = Camera.main.transform.position.z;
+
+        Camera.main.transform.position = pos;
+    }
+}
